756c99f1b5f6134f546013a03ff27598a01ee381
FredFreeb <frederic.fribel@hotmail.fr>
2023-08-03 21:05:07 +0200

    problème images

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+// Importation des classes nécessaires
 use App\Entity\Images;
 use App\Entity\Products;
 use App\Form\ProductsFormType;
@@ -17,69 +18,77 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 #[Route('/admin/produits', name: 'admin_products_')]
 class ProductsController extends AbstractController
 {
+    // Définition de la route par défaut pour l'index des produits dans l'administration
     #[Route('/', name:'index')]
     public function index(): Response
     {
+        // Cette méthode affiche une vue Twig pour la page d'index des produits dans l'administration
         return $this->render('admin/products/index.html.twig');
     }
 
+     // Les autres méthodes (add, edit, delete, deleteImage) sont similaires et gèrent l'ajout, l'édition, la suppression, etc.
+
+    // Méthode pour ajouter un nouveau produit
     #[Route('/ajout', name:'add')]
     public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, PictureService $pictureService): Response
     {
+        // On vérifie si l'utilisateur a le rôle ROLE_ADMIN
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         
-        // on crée un  "nouveau produit"
+        // Création d'une nouvelle instance de la classe Products
         $product = new Products();
 
-        // on crée le formulaire
+        // Création du formulaire en utilisant le ProductsFormType et le produit nouvellement créé
         $productForm = $this->createForm(ProductsFormType::class, $product);
 
-        // on traite la requête du formulaire
+        // Traitement de la requête du formulaire
         $productForm->handleRequest($request);
         
-        // on verifie si le formulaire est soumis est valide
+        // Vérification si le formulaire est soumis et valide
         if($productForm->isSubmitted() && $productForm->isValid()){
-            // On récupère les images
+            // Récupération des images du formulaire
             $images = $productForm->get('images')->getData();
             
+            // Parcours des images et ajout au produit
             foreach($images as $image){
-                // On définit le dossier de destination
+                // Définition du dossier de destination pour les images
                 $folder = 'products';
 
-                // On appelle le service d'ajout
+                // Appel du service pour ajouter l'image avec une taille spécifiée
                 $fichier = $pictureService->add($image, $folder, 300, 300);
 
+                // Création d'une instance de Images pour chaque image
                 $img = new Images();
                 $img->setName($fichier);
                 $product->addImage($img);
             }
             
-            // on génére le slug
+            // Génération du slug à partir du nom du produit
             $slug = $slugger->slug($product->getName());
-
             $product->setSlug($slug);
 
-            // on multiplie le prix pour la database 
-
+            // Conversion du prix en centimes pour la base de données
             $prix = $product->getPrice()*100;
             $product->setPrice($prix);
 
-            // on stocke
+            // Stockage du produit en base de données
             $em->persist($product);
             $em->flush();
 
+            // Ajout d'un message flash pour informer que le produit a été ajouté avec succès
             $this->addFlash('succes','produit ajouté avec succès');
 
-            // on redirige
+            // Redirection vers la liste des produits
             return $this->redirectToRoute('admin_products_index');
         }
 
+        // Affichage du formulaire de création de produit
         return $this->render('admin/products/add.html.twig', [
             'productForm'=>$productForm->createView(),
             'product'=> $product
         ]);
     }
-
+    // Méthode pour modifier un produit
     #[Route('/edition/{id}', name:'edit')]
     public function edit(Products $product,Request $request, EntityManagerInterface $em, SluggerInterface $slugger, PictureService $pictureService): Response
     {
